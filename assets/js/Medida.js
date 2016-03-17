b7d6e972357e7a8173598967d6506ab19307273b
alu0100698688 <alu0100698688@ull.edu.es>
2016-03-17 03:27:45 +0000

    Finalizada estructura y funcionalidad del conversor eliminando switch y usando polimorfismo

--- assets/js/Medida.js
+++ assets/js/Medida.js
@@ -1,64 +1,29 @@
 (function(exports){
   "use strict"
-  var unidad = {
-          medida: 'celsius',
+  function Medida (numero, unidad) {
 
-  }
-
-  var Medida = function (numero, unidadM) {
-
-          unidad.medida = unidadM;
-
-          this.celsius = function () {
-          		alert("Entro");
-              var ce = new Celsius(numero);
-              return ce;
-
-          };
-
-          this.fahrenheit = function () {
-          		alert("En fahrenheit");
-              var ce = new Celsius(numero);
-              return ce;
-
-          };
-
-          this.default = function () {
-              alert('error: no existe esa medida en el programa');
-
-          };
+          this.numero = numero;
+          this.unidad = unidad;
   };
-  Medida.prototype.getNumero=function(){
+  Medida.prototype={
+    getNumero: function(){
       return this.numero;
-  };
+    },
 
-  Medida.prototype.setNumero=function(valor){
+    setNumero: function(valor){
       this.numero = valor;
-  };
+    },
 
-  Medida.prototype.getUnidad=function(){
+    getUnidad: function(){
       return this.unidad;
-  };
+    },
 
-  Medida.prototype.setUnidad=function(valor){
+    setUnidad: function(valor){
       this.unidad = valor;
+    }
   };
 
 
-//     var medidaGeneral = new Medida(32,'fahrenheit');
-// //check that the role is has a strategy
-// if (medidaGeneral[unidad.medida]) {
-//
-//     var m = medidaGeneral[unidad.medida]();
-//     alert("Estoy aqui");
-//     alert(m.getTipo());
-// } else { //otherwise call the default strategy
-//     medidaGeneral['default']();
-// }
-  //Getters y Setters
-
-
-  exports.unidad = unidad;
   exports.Medida = Medida;
 
 })(this);
